2961fc5071004a9cec65d505fcb21db51d542656
Apeksha Vishal <58176648+apeksha-dot@users.noreply.github.com>
2020-09-10 00:13:02 +0530

    Update burgerBuilder.js

--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -31,7 +31,7 @@ export const fetchIngredientsFailed = () => {
 export const initIngredients = () => {
     return dispatch => {
 
-        axios.get( 'https://react-my-burger-a774d.firebaseio.com/ingredients.json' )
+        axios.get( 'https://react-my-burger.firebaseio.com/ingredients.json' )
         .then( response => {
             dispatch(setIngredients(response.data));
          } )
